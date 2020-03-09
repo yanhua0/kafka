3650e276e91472531042a7246112c364337544db
yanhua0 <zjl>
2020-03-09 11:49:21 +0800

    java适配器

--- rpc/src/main/java/org/rpc/service/DealFactory.java
+++ rpc/src/main/java/org/rpc/service/DealFactory.java
@@ -1,9 +1,10 @@
 package org.rpc.service;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.config.BeanPostProcessor;
 
 import java.util.*;
-
+@Slf4j
 public class DealFactory implements BeanPostProcessor {
    private static   Map<String,DealService> dealServiceMap=new HashMap<>();
 
@@ -18,5 +19,42 @@ public class DealFactory implements BeanPostProcessor {
         }
         return bean;
     }
+public static void main(String[] args){
+    /** 创建资源 */
+    Object resourceA = new Object();
+    Object resourceB = new Object();
+    // 创建线程
+    Thread threadA = new Thread(() -> {
+        synchronized (resourceA) {
+            log.info(Thread.currentThread() + " get ResourceA");
+            try {
+                Thread.sleep(1000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            log.info(Thread.currentThread() + "waiting get resourceB");
+            synchronized (resourceB) {
+                log.info(Thread.currentThread() + " get resourceB");
+            }
+        }
+    });
 
+    Thread threadB = new Thread(() -> {
+        synchronized (resourceB) {
+            log.info(Thread.currentThread() + " get ResourceB");
+//            try {
+//               // Thread.sleep(1000);
+//            } catch (InterruptedException e) {
+//               // e.printStackTrace();
+//            }
+            log.info(Thread.currentThread() + "waiting get resourceA");
+            synchronized (resourceA) {
+                log.info(Thread.currentThread() + " get resourceA");
+            }
+        }
+    });
+    threadA.start();
+    threadB.start();
+
+}
 }
